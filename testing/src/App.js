ab4ef0acd03ec96145d398497e211588038f9d72
ArvinDing <dingarvin@gmail.com>
2024-02-17 15:59:42 -0800

    frontend start

--- testing/src/App.js
+++ testing/src/App.js
@@ -1,22 +1,41 @@
 import { socket } from "./socket"
 import React, { Component } from 'react';
+import Bio from './Bio.js'
+
+const PAGES = {
+  HOME: 0,
+  FRIENDS: 1,
+  BIO: 2,
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: PAGES.BIO
+    }
+  }
+
   componentDidMount() {
     socket.on('connect', () => { socket.emit('connected', { data: 'I\'m connected!' }) });
-    socket.on('new person', () => {alert("new")});
+    socket.on('new person', () => { alert("new") });
   }
-  requestCall(){
+
+  requestCall() {
     socket.emit('call', {});
   }
+
   render() {
+    if (this.state.page == PAGES.BIO) {
+      return <Bio></Bio>
+    }
+
     return (
-      
       <div className="App">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <button type="button" onClick={requestCall}>Request Call</button>
+        <button type="button" onClick={this.requestCall}>Request Call</button>
       </div>
     );
   }
---
+++ testing/src/Bio.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class Bio extends Component {
+    render(){
+        return <div>
+            <d1>About Me</d1>
+        </div>
+    }
+}
+export default Bio;
