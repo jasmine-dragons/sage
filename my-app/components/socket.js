aeb220806c163b1b61952e8afe80a1945771e2d2
Nishant Balaji <nishantb1130@gmail.com>
2024-02-18 03:51:04 -0800

    plswork

--- my-app/components/socket.js
+++ my-app/components/socket.js
@@ -2,6 +2,10 @@ import { io } from "socket.io-client";
 
 console.log(process.env.NEXT_PUBLIC_SOCKET_URL);
 const URL = process.env.NEXT_PUBLIC_SOCKET_URL;
-const socket = io(URL);
+const socket = io(URL, {
+  cors: {
+    origin: "*",
+  },
+});
 console.log(socket);
 export default socket;
