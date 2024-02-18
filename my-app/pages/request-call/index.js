3cacf147d16f607cc958d92c1b3beffac7d00e52
Ellie Yang <elliezy@stanford.edu>
2024-02-17 21:23:55 -0800

    add webcam preview

--- my-app/pages/request-call/index.js
+++ my-app/pages/request-call/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Typography } from "@mui/material";
 import './index.css'
+import Webcam from "react-webcam";
 
 const RequestCall = ({
   username,
@@ -20,9 +21,7 @@ const RequestCall = ({
             <img src="https://cdn1.iconfinder.com/data/icons/social-messaging-ui-color-round-2/254000/98-512.png" width="100rem" height="100rem"/>
             <img src="https://cdn.iconscout.com/icon/free/png-256/free-zoom-4062801-3357687.png?f=webp" width="100rem" height="100rem"/>
           </div>
-          <div style={{backgroundColor: 'gray', flex: 5}}>
-            Image of person here
-          </div>
+          <Webcam style={{flex: 4, height: "700%", borderRadius:"1.5rem"}}/>
         </div>
         <div className="right">
           <div className="callButtons" style={{backgroundColor: "#00B84A"}}>
