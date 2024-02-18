00b84ee47f42d75da429d2f7a9b6c1d2a7e96f0f
Nishant Balaji <nishantb1130@gmail.com>
2024-02-17 18:17:13 -0800

    remove comments

--- my-app/pages/Room.js
+++ my-app/pages/Room.js
@@ -1,28 +1,28 @@
 import React, { useState, useEffect } from "react";
 import Video from "twilio-video";
+import Participant from "./Participant";
 
 const Room = ({ roomName, token, handleLogout }) => {
   const [room, setRoom] = useState(null);
   const [participants, setParticipants] = useState([]);
 
-  const remoteParticipants = participants.map((participant) => (
-    <p key={participant.sid}>{participant.identity}</p>
-  ));
-
   useEffect(() => {
     const participantConnected = (participant) => {
       setParticipants((prevParticipants) => [...prevParticipants, participant]);
     };
+
     const participantDisconnected = (participant) => {
       setParticipants((prevParticipants) =>
         prevParticipants.filter((p) => p !== participant)
       );
     };
+
     Video.connect(token, {
       name: roomName,
     }).then((room) => {
       setRoom(room);
       room.on("participantConnected", participantConnected);
+      room.on("participantDisconnected", participantDisconnected);
       room.participants.forEach(participantConnected);
     });
 
@@ -43,15 +43,20 @@ const Room = ({ roomName, token, handleLogout }) => {
     };
   }, [roomName, token]);
 
+  const remoteParticipants = participants.map((participant) => (
+    <Participant key={participant.sid} participant={participant} />
+  ));
+
   return (
     <div className="room">
       <h2>Room: {roomName}</h2>
       <button onClick={handleLogout}>Log out</button>
       <div className="local-participant">
         {room ? (
-          <p key={room.localParticipant.sid}>
-            {room.localParticipant.identity}
-          </p>
+          <Participant
+            key={room.localParticipant.sid}
+            participant={room.localParticipant}
+          />
         ) : (
           ""
         )}
--- my-app/pages/api/server.js
+++ my-app/pages/api/server.js
@@ -1,9 +1,4 @@
 const sendTokenResponse = (token, res) => {
-  console.log(
-    JSON.stringify({
-      token: token.toJwt(),
-    })
-  );
   res.status(200).json(
     JSON.stringify({
       token: token.toJwt(),
