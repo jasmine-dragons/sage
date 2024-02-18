87f96d85f3df86d4b7c151d3ea61eda02a1cc649
Tina <tinatjma@gmail.com>
2024-02-17 18:46:49 -0800

    notification changes

--- testing/src/Notification.js
+++ testing/src/Notification.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
-import "./Notification.css"
+import React, { Component } from "react";
+import "./Notification.css";
 
 class Notification extends Component {
-    render() {
-        return (
-            <div className="pop-up-container">
-                asdasd
-            </div>
-        )
-    }
+  render() {
+    return (
+      <>
+        <div className="right-buttons-container">
+          <button className="answer-button">Answer</button>
+          <button className="decline-button">Decline</button>
+        </div>
+        <div className="green-container">id is calling...</div>;
+      </>
+    );
+  }
 }
 
-export default Notification; 
+export default Notification;
